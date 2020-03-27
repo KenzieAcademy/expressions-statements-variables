9583a93241ee78527719541d8c7409f89d747136
PUT YOUR NAME HEREdwrightdesigns <denise@denisewrightdesigns.com>
2020-03-27 18:03:01 -0500

    first commit

--- global.js
+++ global.js
@@ -7,44 +7,47 @@ console.log(4 + 5)
 /***********************/
 
 //1.) Log an expression that evaluates to the sum of two integers
-// your code here 
-console.log('answer 1');
+let answer1 = 1 + 2; 
+console.log(answer1);
 
 // 2.) log an expression that evaluates to the difference of two integers
-// your code here 
-console.log('answer 2');
+let answer2 = 2 - 1; 
+console.log(answer2);
 
 // 3.) Log an expression that concatenates the two strings "I'm writing" and "my first JavaScript code"
-// your code here 
-console.log('answer 3');
+let sentence = "I'm writing my first Javascript code"
+console.log(sentence)
 
 // 4.) Log an expression that evaluates to true or false using two numbers that are greater or less than eachother
-// your code here 
-console.log('answer 4');
+let answer4 = 5 < 7;
+console.log(answer4);
 
 // 5.) Log an expression that uses modulo, with a result of 4
-// your code here
-console.log('answer 5');
+let answer5 = 32%7
+console.log(answer5);
 
 /**********************/
 /* Part 2: Statements */
 /**********************/
 
 // 6.) Assign a variable and console log the declaration
-// your code here 
-console.log('answer 6');
+let answer6
+console.log(answer6);
 
 // 7.) Console log a variable assigned to the sum of two numbers
-// your code here
-console.log('answer 8');
+let x = 1
+let y = 2
+let answer7 = x + y
+console.log(answer7);
 
 // 8.) Uncomment this if else statement and add a logical expression as the condition
-//if (/*your code here*/) {
-//    console.log('It was!')
-//} else {
-//    console.log('It was not..')
-//}
+let x = 5
+if (x > 7) {
+   console.log('It was!')
+} else {
+   console.log('It was not..')
+}
 
 // 9.) Add parenthesis to the following statement, the result should be 1
-let answer = 3 * 3 + 1 / 8 + 12 / 2;
+let answer = ((3*3) + 1) / ((8+12) / 2);
 console.log(answer == 1); // This should log true
