6066d1667ca44ca5f5ce1591ff48436d45048144
mattayoungs <mattayoungs@icloud.com>
2020-03-27 14:37:33 -0400

    completed assignment

--- global.js
+++ global.js
@@ -1,50 +1,49 @@
-
 // Welcome! Here's an example of a console logged response
-console.log(4 + 5)
+console.log(4 + 5);
 
 /***********************/
 /* Part 1: Expressions */
 /***********************/
 
 //1.) Log an expression that evaluates to the sum of two integers
-// your code here 
-console.log('answer 1');
+ans1 = 1 + 2;
+console.log(ans1);
 
 // 2.) log an expression that evaluates to the difference of two integers
-// your code here 
-console.log('answer 2');
+ans2 = 2 - 1;
+console.log(ans2);
 
 // 3.) Log an expression that concatenates the two strings "I'm writing" and "my first JavaScript code"
-// your code here 
-console.log('answer 3');
+console.log("I'm writing" + "my first Javascript code");
 
 // 4.) Log an expression that evaluates to true or false using two numbers that are greater or less than eachother
-// your code here 
-console.log('answer 4');
+ans4 = 5 > 4;
+console.log(ans4);
 
 // 5.) Log an expression that uses modulo, with a result of 4
-// your code here
-console.log('answer 5');
+ans5 = 24 % 5;
+console.log(ans5);
 
 /**********************/
 /* Part 2: Statements */
 /**********************/
 
 // 6.) Assign a variable and console log the declaration
-// your code here 
-console.log('answer 6');
+ans6 = "neato";
+console.log(ans6);
 
 // 7.) Console log a variable assigned to the sum of two numbers
-// your code here
-console.log('answer 8');
+ans7 = 5 + 1;
+console.log(ans7);
 
 // 8.) Uncomment this if else statement and add a logical expression as the condition
-//if (/*your code here*/) {
-//    console.log('It was!')
-//} else {
-//    console.log('It was not..')
-//}
+if (5 > 4) {
+  console.log("It was!");
+} else {
+  console.log("It was not..");
+}
 
-// 9.) Add parenthesis to the following statement, the result should be 1
+// 9.) Add parenthesis to the following statement, the result should be 12.125 ;)
 let answer = 2 * 3 + 1 / 8 + 12 / 2;
-console.log(answer == 1); // This should log true
+console.log(answer);
+console.log(answer == 12.125); // This should log true
