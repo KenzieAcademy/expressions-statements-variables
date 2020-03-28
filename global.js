f62dbd5fe12a48f68c1d54925d371f884023646e
EthanYosh <ehnohoa@gmail.com>
2020-03-28 01:23:43 -0400

    finished all the problems and the scary #9

--- global.js
+++ global.js
@@ -1,50 +1,60 @@
 
 // Welcome! Here's an example of a console logged response
-console.log(4 + 5)
+console.log(4 + 5);
 
 /***********************/
 /* Part 1: Expressions */
 /***********************/
 
 //1.) Log an expression that evaluates to the sum of two integers
-// your code here 
-console.log('answer 1');
+let a = 2;
+let b = 1;
+console.log(a+b);
 
 // 2.) log an expression that evaluates to the difference of two integers
-// your code here 
-console.log('answer 2');
+let c = 5;
+let d = 2;
+console.log(c-d);
 
 // 3.) Log an expression that concatenates the two strings "I'm writing" and "my first JavaScript code"
-// your code here 
-console.log('answer 3');
+let e = "I'm writing";
+let f = "my first JavaScript code";
+console.log(e,f);
 
 // 4.) Log an expression that evaluates to true or false using two numbers that are greater or less than eachother
 // your code here 
-console.log('answer 4');
+let g = 23;
+let h = -23;
+console.log(g>h);
 
 // 5.) Log an expression that uses modulo, with a result of 4
 // your code here
-console.log('answer 5');
+let i = 9;
+let j = 5;
+console.log(i%j);
 
 /**********************/
 /* Part 2: Statements */
 /**********************/
 
 // 6.) Assign a variable and console log the declaration
-// your code here 
-console.log('answer 6');
+let k = 897657;
+k = 2;
+console.log(k);
 
 // 7.) Console log a variable assigned to the sum of two numbers
-// your code here
-console.log('answer 8');
+let l = 5;
+let m = 3;
+let n = l+m;
+console.log(n);
 
 // 8.) Uncomment this if else statement and add a logical expression as the condition
-//if (/*your code here*/) {
-//    console.log('It was!')
-//} else {
-//    console.log('It was not..')
-//}
+if (n > k) {
+    console.log('It was!')
+} else {
+    console.log('It was not..')
+}
 
 // 9.) Add parenthesis to the following statement, the result should be 1
-let answer = 3 * 3 + 1 / 8 + 12 / 2;
+let answer = (3 * 3 + 1) / ((8 + 12) / 2);
 console.log(answer == 1); // This should log true
