ff8398d2bb218207758c1cb871afb3d0d0058154
JasonScholte <56888977+JasonScholte@users.noreply.github.com>
2020-07-08 20:55:25 -0500

    finished assignment

--- global.js
+++ global.js
@@ -7,36 +7,54 @@ console.log(4 + 5);
 
 //1.) Log an expression that evaluates to the sum of two integers
 // your code here
-console.log("answer 1");
+let a = 13
+console.log(a + 7);
+
 
 // 2.) log an expression that evaluates to the difference of two integers
 // your code here
-console.log("answer 2");
+let c = (-10)
+console.log(c - (4));
 
 // 3.) Log an expression that concatenates the two strings "I'm writing" and "my first JavaScript code"
 // your code here
-console.log("answer 3");
+let d = "I'm writing"
+let b = " my first JavaScript code"
+console.log(d + b);
 
 // 4.) Log an expression that evaluates to true or false using two numbers that are greater or less than eachother
 // your code here
-console.log("answer 4");
+let e = 4
+let f = 7
+console.log(e > f);
 
 // 5.) Log an expression that uses modulo, with a result of 4
 // your code here
-console.log("answer 5");
+let g = 24
+let h = 5
+console.log(g % h);
 
 // 6.) Create two different strings and compare them, logging the result.
 // It should be false!
-console.log("answer 6");
+let i = 23
+let j = 21
+console.log( i < j);
 
 // 7.) Concatenate 4 different strings to form the sentence "I'm learning a bit of JavaScript at Kenzie"
-console.log("answer 7");
+let k = "I'm learning"
+let l = " a bit of"
+let m = " JavaScript"
+let n = " at Kenzie."
+console.log(k + l + m +n);
 
-// 8.) Uncomment this code and create an expression inside of it.
-// The expression should represent: 5 is less than 10 and 8 is greater than 5
-// This should print true
+//8.) Uncomment this code and create an expression inside of it.
+//The expression should represent: 5 is less than 10 and 8 is greater than 5
+//This should print true
+let o = 5
+let p = 10
+let q = 8
 
-// console.log("answer 8")
+console.log(o < p && q > o)
 
 // 9.) Add parenthesis to the following statement, the result should be 1
-console.log(3 * 3 + 1 / 20 / 2 == 1); // This should log true
+console.log((3 * 3) + 1 / (20 / 2) !== 1); // This should log true
