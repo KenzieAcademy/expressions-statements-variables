6c2202438c1afd7e5269b486fb3218a2045ad01d
megumi29 <megumisan90@gmail.com>
2020-07-10 11:43:10 -0400

    re-added original questions 5 & 9 back into code and corrected answers

--- global.js
+++ global.js
@@ -17,21 +17,28 @@ console.log("answer 3", "I'm writing" + " " + "my first JavaScript code");
 // 4.) Log an expression that evaluates to true or false using two numbers that are greater or less than eachother
 console.log("answer 4", 6 < 7);
 
+// 5.) Log an expression that uses modulo, with a result of 4
+console.log("answer 5", 32 % 7);
+
 /**********************/
 /* Part 2: Statements */
 /**********************/
 
-// 5.) Assign a variable and console log the declaration
-let answer5 = 8;
-console.log(answer5);
+// 6.) Assign a variable and console log the declaration
+let answer6 = 8;
+console.log(answer6);
 
-// 6.) Console log a variable assigned to the sum of two numbers
-let answer6 = 20 + 14;
-console.log("answer 6", answer6);
+// 7.) Console log a variable assigned to the sum of two numbers
+let answer7 = 20 + 14;
+console.log("answer 7", answer7);
 
-// 7.) Uncomment this if else statement and add a logical expression as the condition
+// 8.) Uncomment this if else statement and add a logical expression as the condition
 if (5 < 4) {
   console.log("It was!");
 } else {
   console.log("It was not..");
 }
+
+// 9.) Add parenthesis to the following statement, the result should be 1
+let answer = (3 * 3 + 1) / (20 / 2);
+console.log(answer == 1); // This should log true
