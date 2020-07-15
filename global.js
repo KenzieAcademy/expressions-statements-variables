4f1110b8b584c730cdcfd6e07afacb5a96552550
morganjwilson <64968479+morganjwilson@users.noreply.github.com>
2020-07-15 18:43:37 -0400

    Finishing

--- global.js
+++ global.js
@@ -7,36 +7,47 @@ console.log(4 + 5);
 
 //1.) Log an expression that evaluates to the sum of two integers
 // your code here
-console.log("answer 1");
+console.log(3 + 2);
 
 // 2.) log an expression that evaluates to the difference of two integers
 // your code here
-console.log("answer 2");
+console.log(10 - 3);
 
 // 3.) Log an expression that concatenates the two strings "I'm writing" and "my first JavaScript code"
-// your code here
-console.log("answer 3");
+//
+var str1 = "I'm writing ";
+var str2 = "my first JavaScript code.";
+var res = str1.concat(str2);
+
+console.log("I'm writing my first JavaScript code");
 
 // 4.) Log an expression that evaluates to true or false using two numbers that are greater or less than eachother
 // your code here
-console.log("answer 4");
+console.log(100 > 25);
 
 // 5.) Log an expression that uses modulo, with a result of 4
 // your code here
-console.log("answer 5");
+console.log(14 % 5);
 
 // 6.) Create two different strings and compare them, logging the result.
 // It should be false!
-console.log("answer 6");
+var str1 = 3;
+var str2 = 5;
+console.log(str1 > str2);
 
 // 7.) Concatenate 4 different strings to form the sentence "I'm learning a bit of JavaScript at Kenzie"
-console.log("answer 7");
+var str1 = "I'm learning ";
+var str2 = "a bit of ";
+var str3 = "JavaScript ";
+var str4 = "at Kenzie. ";
+var res = str1.concat(str2, str3, str4);
+console.log();
 
-// 8.) Uncomment this code and create an expression inside of it.
+console.log(8 > 5 < 10);
 // The expression should represent: 5 is less than 10 and 8 is greater than 5
 // This should print true
 
 // console.log("answer 8")
 
 // 9.) Add parenthesis to the following statement, the result should be 1
-console.log(3 * 3 + 1 / 20 / 2 == 1); // This should log true
+console.log(3 * 3 + 1(20 / 2) == 1); // This should log true
